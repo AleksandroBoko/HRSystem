be28ca991297aa6b89e2cb7fa1cf370ca7e200a1
Aleks B <aleksandroboko@gmail.com>
2018-02-02 18:07:33 +0200

    Add position repository and EF to the WEB project

---
+++ HRSystem.DataAccess/Repository/Implementation/PositionRepository.cs
@@ -0,0 +1,75 @@
+﻿using HRSystem.DataAccess.Entity;
+using System;
+using System.Linq;
+
+namespace HRSystem.DataAccess.Repository.Implementation
+{
+    public class PositionRepository : IRepository<Position>
+    {
+        public PositionRepository()
+        {
+            context = new HRModel();
+        }
+
+        private readonly HRModel context;
+
+        public Guid Add(Position item)
+        {
+            if(item == null)
+            {
+                throw new ArgumentNullException("The argument is null");
+            }
+
+            var position = context.Positions.Add(item);
+            var positionId = position.Id;
+            Save();
+            return positionId;
+        }
+
+        public IQueryable<Position> GetAllItems()
+        {
+            return context.Positions;
+        }
+
+        public Position GetItemById(Guid id)
+        {
+            return context.Positions.FirstOrDefault(x => x.Id == id);
+        }
+
+        public Guid Remove(Guid id)
+        {
+            var position = context.Positions.FirstOrDefault(x => x.Id == id);
+            if (position == null)
+            {
+                throw new NullReferenceException("The position wasn't found");
+            }
+
+            context.Positions.Remove(position);
+            Save();
+            return id;
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        public Guid Update(Position item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException("The argument is null");
+            }
+
+            var position = context.Positions.FirstOrDefault(x => x.Id == item.Id);
+            if (position == null)
+            {
+                throw new NullReferenceException("The position wasn't found");
+            }
+
+            position.Name = item.Name;
+            Save();
+            return item.Id;
+        }
+    }
+}
