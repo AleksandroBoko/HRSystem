2d80e5248f3fa573296dbd0b337a7f8f6876ac6b
Aleks B <aleksandroboko@gmail.com>
2018-02-02 14:00:22 +0200

    Add common interface for concrete repositories

---
+++ HRSystem.DataAccess/Repository/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace HRSystem.DataAccess.Repository
+{
+    public interface IRepository<T>
+    {
+        T GetItemById(Guid id);
+        IQueryable<T> GetAllItems();
+        Guid Add(T item);
+        Guid Update(T item);
+        Guid Remove(Guid id);
+        void Save();
+    }
+}
