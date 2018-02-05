27a05b68a934912c9d363a0aeb7d3da4ee1a6e18
Aleks B <aleksandroboko@gmail.com>
2018-02-05 11:27:03 +0200

    Add repository for Specialization entity

---
+++ HRSystem.DataAccess/Repository/Implementation/SpecializationRepository.cs
@@ -0,0 +1,79 @@
+﻿using HRSystem.DataAccess.Entity;
+using System;
+using System.Linq;
+
+namespace HRSystem.DataAccess.Repository.Implementation
+{
+    class SpecializationRepository : IRepository<Specialization>
+    {
+        public SpecializationRepository()
+        {
+            context = new HRModel();
+        }
+
+        private readonly HRModel context;
+
+        public Guid Add(Specialization item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException("The argument is null");
+            }
+
+            var specialization = context.Specializations.Add(item);
+            var specializationId = specialization.Id;
+            Save();
+            return specializationId;
+        }
+
+        public IQueryable<Specialization> GetAllItems()
+        {
+            return context.Specializations;
+        }
+
+        public Specialization GetItemById(Guid id)
+        {
+            return context.Specializations.FirstOrDefault(x => x.Id == id);
+        }
+
+        public Guid Remove(Guid id)
+        {
+            var specialization = context.Specializations.FirstOrDefault(x => x.Id == id);
+            if(specialization == null)
+            {
+                throw new NullReferenceException("The specialization wasn't found");
+            }
+
+            var removedSpecialization = context.Specializations.Remove(specialization);
+            if (removedSpecialization == null)
+            {
+                throw new NullReferenceException("The specialization wasn't removed");
+            }
+            Save();
+            return removedSpecialization.Id;
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        public Guid Update(Specialization item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException("The argument is null");
+            }
+
+            var specialization = context.Specializations.FirstOrDefault(x => x.Id == item.Id);
+            if (specialization == null)
+            {
+                throw new NullReferenceException("The specialization wasn't found");
+            }
+
+            specialization.Name = item.Name;
+            Save();
+            return item.Id;
+        }
+    }
+}
