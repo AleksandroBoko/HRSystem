7742aa7f6310429302d99577a74a27dd47c49a9f
Aleks B <aleksandroboko@gmail.com>
2018-02-05 12:45:30 +0200

    Add repository for entity Technology

---
+++ HRSystem.DataAccess/Repository/Implementation/TechnologyRepository.cs
@@ -0,0 +1,80 @@
+﻿using HRSystem.DataAccess.Entity;
+using System;
+using System.Linq;
+
+namespace HRSystem.DataAccess.Repository.Implementation
+{
+    class TechnologyRepository : IRepository<Technology>
+    {
+        public TechnologyRepository()
+        {
+            context = new HRModel();
+        }
+
+        private readonly HRModel context;
+
+        public Guid Add(Technology item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException("The argument is null");
+            }
+
+            var technnology = context.Technologies.Add(item);
+            var technnologytId = technnology.Id;
+            Save();
+            return technnologytId;
+        }
+
+        public IQueryable<Technology> GetAllItems()
+        {
+            return context.Technologies;
+        }
+
+        public Technology GetItemById(Guid id)
+        {
+            return context.Technologies.FirstOrDefault(x => x.Id == id);
+        }
+
+        public Guid Remove(Guid id)
+        {
+            var technology = context.Technologies.FirstOrDefault(x => x.Id == id);
+            if (technology == null)
+            {
+                throw new NullReferenceException("The technology wasn't found");
+            }
+
+            var removedTechnology = context.Technologies.Remove(technology);
+            if (removedTechnology == null)
+            {
+                throw new NullReferenceException("The technology wasn't removed");
+            }
+            Save();
+            return removedTechnology.Id;
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        public Guid Update(Technology item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException("The argument is null");
+            }
+
+            var technology = context.Technologies.FirstOrDefault(x => x.Id == item.Id);
+            if (technology == null)
+            {
+                throw new NullReferenceException("The technology wasn't found");
+            }
+
+            technology.Name = item.Name;
+            technology.Description = item.Description;
+            Save();
+            return item.Id;
+        }
+    }
+}
